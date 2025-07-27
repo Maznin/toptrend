41082fc66ad01db3cf2d49ed31d29d8d4a6027f3
Marko Mladenovic <marko.mladenovic988@gmail.com>
2025-07-27 17:27:08 +0200

    remove: templates cleanup

--- wp-content/themes/frizer/nav-below.php
+++
@@ -1,5 +0,0 @@
-<?php $args = array(
-'prev_text' => sprintf( esc_html__( '%s older', 'frizer' ), '<span class="meta-nav">&larr;</span>' ),
-'next_text' => sprintf( esc_html__( 'newer %s', 'frizer' ), '<span class="meta-nav">&rarr;</span>' )
-);
-the_posts_navigation( $args );
