063a6a1131cb18e5453f3d884e514f947d4c9937
Marko Mladenovic <marko.mladenovic988@gmail.com>
2025-07-27 19:49:06 +0200

    templates cleanup

--- wp-content/themes/frizer/footer.php
+++ wp-content/themes/frizer/footer.php
@@ -1,12 +1,9 @@
 </main>
-<?php get_sidebar(); ?>
-</div>
 <footer id="footer" role="contentinfo">
     <div id="copyright">
         &copy; <?php echo esc_html(date_i18n(__('Y', 'frizer'))); ?> <?php echo esc_html(get_bloginfo('name')); ?>
     </div>
 </footer>
-</div>
 <?php wp_footer(); ?>
 </body>
 
