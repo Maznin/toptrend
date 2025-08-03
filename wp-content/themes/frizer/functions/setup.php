383d6923ab808139d77cee74927584d3262623a1
Marko Mladenovic <marko.mladenovic988@gmail.com>
2025-08-03 14:39:38 +0200

    update: function name

--- wp-content/themes/frizer/functions/setup.php
+++ wp-content/themes/frizer/functions/setup.php
@@ -45,8 +45,8 @@ add_action('wp_body_open', 'frizer_skip_link', 5);
  * This function registers support for block template parts, allowing the theme to use
  * block-based template parts in its design.
  */
-function hybrid_add_template_parts_support() {
+function frizer_add_template_parts_support() {
   add_theme_support( 'block-template-parts' );
 }
-add_action( 'after_setup_theme', 'hybrid_add_template_parts_support' );
+add_action( 'after_setup_theme', 'frizer_add_template_parts_support' );
 
