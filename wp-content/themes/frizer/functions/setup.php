b104c6ec63c9cf7b315d7c21d4605053cae55ac4
Marko Mladenovic <marko.mladenovic988@gmail.com>
2025-08-26 09:37:52 +0200

    add: register new footer menu

--- wp-content/themes/frizer/functions/setup.php
+++ wp-content/themes/frizer/functions/setup.php
@@ -35,7 +35,7 @@ if (!function_exists('frizer_wp_body_open')) {
  * It is hooked to the 'wp_body_open' action with a priority of 5, ensuring it appears early in the body.
  */
 function frizer_skip_link() {
-    echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__('Skip to the content', 'frizer') . '</a>';
+  echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__('Skip to the content', 'frizer') . '</a>';
 }
 add_action('wp_body_open', 'frizer_skip_link', 5);
 
@@ -47,4 +47,9 @@ function frizer_add_template_parts_support() {
   add_theme_support( 'menus' );
 }
 add_action( 'after_setup_theme', 'frizer_add_template_parts_support' );
+
+function frizer_register_footer_menu() {
+  register_nav_menu('footer', __('Footer Menu', 'frizer'));
+}
+add_action('after_setup_theme', 'frizer_register_footer_menu');
 
